963c89c27ab38bbd0ab9bc8588477dbb2c6d9aa2
Lara Rodríguez <lararh393@gmail.com>
2024-09-19 21:28:30 +0200

    Mayuscula

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./routes/Layout/Layout";
 import Home from "./routes/Home/Home";
-import Signup from "./routes/Signup/Signup";
+import SignUp from "./routes/Signup/Signup";
 import Login from "./routes/Login/Login";
 import NewPost from "./routes/NewPost/NewPost";
 import Update from "./routes/Update/Update.jsx";
@@ -17,7 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/register" element={<signUp />} />
+          <Route path="/register" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
           <Route path="/newentry" element={<NewPost />} />
           <Route path="/entry/:id" element={<Update />} />
